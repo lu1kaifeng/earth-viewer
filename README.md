9c9ec40514ff89cec847fe544bd919758978c0f0
lu1kaifeng <11407836+lu1kaifeng@users.noreply.github.com>
2022-04-30 14:03:45 +0800

    add Chinese Tile Service

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Tinkering with Three.js for GIS Systems
+# Tinkering with JavaScript ArcGIS API
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
 
 ## Available Scripts
--- src/gis/CustomLayer.tsx
+++ src/gis/CustomLayer.tsx
@@ -2,7 +2,7 @@ import WebTileLayer from "@arcgis/core/layers/WebTileLayer";
 import BaseTileLayer from "@arcgis/core/layers/BaseTileLayer";
 
 export class CustomLayer extends  BaseTileLayer{
-    title="Baidu"
+    title="GaoDe"
     getTileUrl(level: number, row: number, col: number): string {
 
         return "http://webst04.is.autonavi.com/appmaptile?style=6&x="+col+"&y="+row+"&z="+level
